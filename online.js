ad9114ca0e68cf46ba7c2174b57a38f824a1ac87
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-12-05 17:10:22 +0700

    Update online.js

--- online.js
+++ online.js
@@ -9,18 +9,18 @@ client.on('message', message => {
 	
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
+client.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '/etzh') {
+  if (message.content === '/join') {
     // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
     } else {
       message.reply('You need to join a voice channel first!');
     }
   }
+});
 });
